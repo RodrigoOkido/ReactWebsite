4a77957e586fe2e34795a7cc9b111a07b73ef14e
RodrigoOkido <rodrigookido@gmail.com>
2020-10-12 19:08:41 -0300

    updated navbar hrefs references

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,16 +13,16 @@ class Navbar extends React.Component {
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                   <li className="nav-item active">
-                    <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="./">Home <span className="sr-only">(current)</span></a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="/#client">Our Clients</a>
+                    <a className="nav-link" href="./#client">Our Clients</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="/#about">About Us </a>
+                    <a className="nav-link" href="./#about">About Us </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="/#contact">Contact</a>
+                    <a className="nav-link" href="./#contact">Contact</a>
                   </li>
                 </ul>
               </div>
