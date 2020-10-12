054c978390365c013aa1683f4b4906fcff3d2a23
RodrigoOkido <rodrigookido@gmail.com>
2020-10-12 18:34:24 -0300

    Updates

---
+++ precache-manifest.9814a957ce3392983184660c10bd2188.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df953039476c54a9ff318ba48bafba3f",
+    "url": "/ReactWebsite/index.html"
+  },
+  {
+    "revision": "ec684bf24fdac05fce35",
+    "url": "/ReactWebsite/static/css/main.8cb0c888.chunk.css"
+  },
+  {
+    "revision": "38af9464885989625e40",
+    "url": "/ReactWebsite/static/js/2.bc70ce57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactWebsite/static/js/2.bc70ce57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec684bf24fdac05fce35",
+    "url": "/ReactWebsite/static/js/main.6df1dd88.chunk.js"
+  },
+  {
+    "revision": "f9432f4e5ae72c54c1a8",
+    "url": "/ReactWebsite/static/js/runtime-main.f7cae7df.js"
+  },
+  {
+    "revision": "84e7f4e12f580857d052c0a3b7de3e99",
+    "url": "/ReactWebsite/static/media/city.84e7f4e1.jpg"
+  }
+]);
